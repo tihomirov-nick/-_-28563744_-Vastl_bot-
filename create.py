672bf2071df16aecdcaf60bdeeb587bd71dba75a
tihomirov-nick <111696823+tihomirov-nick@users.noreply.github.com>
2023-02-18 14:53:52 +0300

    Update create.py

--- create.py
+++ create.py
@@ -6,5 +6,5 @@ from aiogram.contrib.fsm_storage.memory import MemoryStorage
 storage=MemoryStorage()
 
 # bot = Bot(token=os.getenv('TOKEN'))
-bot = Bot(token=('5136198355:AAGwHHpaOi6vuMPF8eyczIGhQJJQ9W7ZYQU'))
+bot = Bot(token=('TOKEN'))
 dp = Dispatcher(bot, storage=storage)
